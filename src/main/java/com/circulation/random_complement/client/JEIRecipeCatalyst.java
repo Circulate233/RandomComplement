de2a870812855a05b970d3a06fbadb0482fafeba
Circulate233 <a18966009058@163.com>
2025-02-11 19:48:08 +0800

    将Extended Crafting Terminals for Applied Energistics 2的终端也添加到对应的JEI催化剂位置

--- src/main/java/com/circulation/random_complement/client/JEIRecipeCatalyst.java
+++ src/main/java/com/circulation/random_complement/client/JEIRecipeCatalyst.java
@@ -1,6 +1,9 @@
 package com.circulation.random_complement.client;
 
+import com._0xc4de.ae2exttable.items.ItemRegistry;
+import com.blakebr0.extendedcrafting.compat.jei.tablecrafting.*;
 import mezz.jei.api.*;
+import mezz.jei.api.recipe.VanillaRecipeCategoryUid;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
@@ -9,7 +12,7 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 import org.jetbrains.annotations.NotNull;
 
 @JEIPlugin
-public class packagedJEI implements IModPlugin {
+public class JEIRecipeCatalyst implements IModPlugin {
 
     public static IModRegistry registration;
 
@@ -18,7 +21,7 @@ public class packagedJEI implements IModPlugin {
      */
     @Override
     public void register(final @NotNull IModRegistry registry) {
-        packagedJEI.registration = registry;
+        JEIRecipeCatalyst.registration = registry;
         if (Loader.isModLoaded("packagedexcrafting")) {
             registration.addRecipeCatalyst(getOtherModsItemStack("packagedexcrafting", "combination_crafter"), "extendedcrafting:combination_crafting");
             registration.addRecipeCatalyst(getOtherModsItemStack("packagedexcrafting", "marked_pedestal"), "extendedcrafting:combination_crafting");
@@ -39,6 +42,25 @@ public class packagedJEI implements IModPlugin {
         if (Loader.isModLoaded("packageddraconic")) {
             registration.addRecipeCatalyst(getOtherModsItemStack("packageddraconic", "fusion_crafter"), "DraconicEvolution.Fusion");
         }
+        if (Loader.isModLoaded("ae2exttable")) {
+            addExtended();
+        }
+    }
+
+    public void addExtended(){
+        registration.addRecipeCatalyst(ItemRegistry.BASIC_TERMINAL.getDefaultInstance(), VanillaRecipeCategoryUid.CRAFTING);
+        registration.addRecipeCatalyst(ItemRegistry.BASIC_TERMINAL.getDefaultInstance(), BasicTableCategory.UID);
+        registration.addRecipeCatalyst(ItemRegistry.WIRELESS_BASIC_TERMINAL.getDefaultInstance(),VanillaRecipeCategoryUid.CRAFTING);
+        registration.addRecipeCatalyst(ItemRegistry.WIRELESS_BASIC_TERMINAL.getDefaultInstance(),BasicTableCategory.UID);
+
+        registration.addRecipeCatalyst(ItemRegistry.ADVANCED_TERMINAL.getDefaultInstance(),AdvancedTableCategory.UID);
+        registration.addRecipeCatalyst(ItemRegistry.WIRELESS_ADVANCED_TERMINAL.getDefaultInstance(),AdvancedTableCategory.UID);
+
+        registration.addRecipeCatalyst(ItemRegistry.ELITE_TERMINAL.getDefaultInstance(),EliteTableCategory.UID);
+        registration.addRecipeCatalyst(ItemRegistry.WIRELESS_ELITE_TERMINAL.getDefaultInstance(),EliteTableCategory.UID);
+
+        registration.addRecipeCatalyst(ItemRegistry.ULTIMATE_TERMINAL.getDefaultInstance(),UltimateTableCategory.UID);
+        registration.addRecipeCatalyst(ItemRegistry.WIRELESS_ULTIMATE_TERMINAL.getDefaultInstance(),UltimateTableCategory.UID);
     }
 
     public static ItemStack getOtherModsItemStack(String modId, String itemName) {
