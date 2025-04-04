0f42fa4b7571c1be59af30b629fae83ddaa746a1
Circulate233 <a18966009058@163.com>
2025-04-04 14:59:24 +0800

    奇异的mixin错误，暂时更换为反射调用

--- src/main/java/com/circulation/random_complement/mixin/mmce/MixinStructurePreviewWrapper.java
+++ src/main/java/com/circulation/random_complement/mixin/mmce/MixinStructurePreviewWrapper.java
@@ -12,43 +12,51 @@ import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 @Mixin(value = StructurePreviewWrapper.class,remap = false)
 public class MixinStructurePreviewWrapper {
+
     @Final
     @Shadow
     private DynamicMachine machine;
     @Shadow
     private GuiBlueprintScreenJEI gui;
+    @Unique
+    private static Field randomComplement$MachineStructurePreviewPanelField;
+
+    static {
+        try {
+            randomComplement$MachineStructurePreviewPanelField = MachineStructurePreviewPanel.class.getDeclaredField("renderer");
+            randomComplement$MachineStructurePreviewPanelField.setAccessible(true);
+        } catch (NoSuchFieldException ignored) {
+
+        }
+    }
 
     @Redirect(method = "getIngredients",at = @At(value = "INVOKE", target = "Lhellfirepvp/modularmachinery/common/machine/TaggedPositionBlockArray;getIngredientList()Ljava/util/List;"))
     public List<List<ItemStack>> getIngredientsMixin(TaggedPositionBlockArray instance){
         if (gui != null) {
-            WidgetController controller = this.gui.getWidgetController();
-            var panel = ((AccessorMachineStructurePreviewPanel)PreviewPanels.getPanel(this.machine, controller.getGui())).getRenderer();
-            var list = panel.getPattern().getDescriptiveStackList(panel.getTickSnap(), panel.getWorldRenderer().getWorld(), panel.getRenderOffset());
-            list.remove(0);
-            List<List<ItemStack>> finalList = new ArrayList<>();
-            list.forEach(itemStack -> finalList.add(Collections.singletonList(itemStack)));
-            return finalList;
-
+            try {
+                WidgetController controller = this.gui.getWidgetController();
+                var panel = (WorldSceneRendererWidget)randomComplement$MachineStructurePreviewPanelField.get(PreviewPanels.getPanel(this.machine, controller.getGui()));
+                var list = panel.getPattern().getDescriptiveStackList(panel.getTickSnap(), panel.getWorldRenderer().getWorld(), panel.getRenderOffset());
+                list.remove(0);
+                List<List<ItemStack>> finalList = new ArrayList<>();
+                list.forEach(itemStack -> finalList.add(Collections.singletonList(itemStack)));
+                return finalList;
+            } catch (IllegalAccessException e) {
+                return instance.getIngredientList();
+            }
         }
         return instance.getIngredientList();
-    }
-
-    @Mixin(value = MachineStructurePreviewPanel.class,remap = false)
-    public interface AccessorMachineStructurePreviewPanel{
-
-        @Accessor
-        WorldSceneRendererWidget getRenderer();
-
     }
 
 }
